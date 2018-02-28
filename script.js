10470206d8cfa885ba18b602c45ab252834c99b0
= <druan10@gmail.com>
2018-02-27 22:01:49 -0800

    Simple temperature and location feature added

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 
 var x = document.getElementById("#weatherText");
+var tempScale = "farenheit";
+
+$("#weatherDiv").children().hide();
+$("#weatherDiv").children().fadeIn();
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showWeather);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        $("#weatherDiv").innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-function showPosition(position) {
+function showWeather(position) {
 
     $.ajax({
         url: "https://fcc-weather-api.glitch.me//api/current?",
-        success: function(data) {
-            
+        success: function (data) {
+
+            $("#weatherDiv").children().hide();
+
+            var temp;
+
+            if (tempScale == "farenheit") {
+                temp = ((data.main.temp * 9) / 5) + 32;
+            } else {
+                temp = data.main.temp;
+            }
+
+            $("#weatherDiv").html(
+                `
+                <h1 class="text-center">Current Weather in ` + data.name + ` is</h1> 
+                <h3>`+ temp + ` degrees ` + tempScale + `</h3>
+
+                `
+            );
         },
-        data : {
+        data: {
             lon: position.coords.longitude.toFixed(2),
             lat: position.coords.latitude.toFixed(2)
         }
-      });
+    });
 }
 
 $("#updateWeather").click(function () {
     getLocation();
 });
+
